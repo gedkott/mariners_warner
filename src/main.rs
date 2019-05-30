7080b48482971e8289ddeaecd904acfc92c39d4c
Gedalia Kott <gedkott@gmail.com>
2019-05-30 01:34:40 -0700

    add logging infrastructure and refactor twilio module
    moves towards a executor model where the caller specifies the object
    that will actually execute the sending ofthe message
    this is to enable easier testing
    I used traits and emtpy structs to make different static implementations
    of executors (one real which is the original, and one fae for tests)
    
    now we can more easily capture errors from the execution process and
    log as needed to a file for later investigation
    
    note that the API for executors is pretty low level which means that
    callers handling the return value from an execute call need to transform
    the value into the domain layer's model (value or error)
    It is not a clean idea yet, but suffices for now

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,14 @@ extern crate chrono;
 extern crate config;
 extern crate time;
 
+#[macro_use]
+extern crate log;
+extern crate simplelog;
+
 use chrono::{FixedOffset, Utc};
+use simplelog::*;
 use std::collections::HashMap;
+use std::fs::File;
 use std::{fs, sync, thread, time as native_time};
 
 mod async_latch;
@@ -12,6 +18,13 @@ mod game_parser;
 mod twilio;
 
 fn main() {
+    CombinedLogger::init(vec![WriteLogger::new(
+        LevelFilter::Info,
+        Config::default(),
+        File::create("mariners_warner.log").unwrap(),
+    )])
+    .expect("could not initialize logging infrastructure");
+
     let mut settings = config::Config::default();
     settings.merge(config::File::with_name("config")).expect(
         "you must supply a config file named config.toml matching config.template.toml's structure",
@@ -69,7 +82,7 @@ fn main() {
                         None
                     } else {
                         Some(sync::Arc::new(move || {
-                            println!(
+                            info!(
                                 "sleeping for {:?} (u64) for game on {:?} at {:?}",
                                 time_to_sleep as u64,
                                 start_date_time
@@ -85,7 +98,11 @@ fn main() {
                                 &to,
                                 &twilio_account_id,
                                 &twilio_access_token,
-                            );
+                                &twilio::CommandExecutor,
+                            )
+                            .map(|response| info!("{:?}", response))
+                            .map_err(|e| error!("{:?}", e))
+                            .ok();
                         }))
                     }
                 }
--- src/twilio.rs
+++ src/twilio.rs
@@ -2,29 +2,75 @@ use std::process::Command;
 
 use serde::{Deserialize, Serialize};
 
+#[derive(Debug, PartialEq)]
+pub enum SMSError {
+    TwilioResponseError { error: String },
+    SerdeError { error: String },
+    ExecutionError { error: String },
+}
+
+pub trait SMSExecutor {
+    fn execute(
+        &self,
+        from: &str,
+        to: &str,
+        account_id: &str,
+        access_token: &str,
+    ) -> Result<std::process::Output, std::io::Error>;
+}
+
 pub fn send_text_message(
     from: &str,
     to: &str,
     account_id: &str,
     access_token: &str,
-) -> TwilioResponse {
-    let output = Command::new("sh")
-        .arg("-c")
-        .arg(format!(
-            "
-curl -X POST https://api.twilio.com/2010-04-01/Accounts/{}/Messages.json \
-    --data-urlencode \"Body=A Mariners Home Game is Starting\" \
-    --data-urlencode \"From=+1{}\" \
-    --data-urlencode \"To=+1{}\" \
-    -u {}:{}
-            ",
-            account_id, from, to, account_id, access_token
-        ))
-        .output()
-        .expect("failed to execute process");
-    let response: TwilioResponse =
-        serde_json::from_str(&String::from_utf8(output.stdout.to_vec()).unwrap()).unwrap();
-    response
+    sms_executor: &SMSExecutor,
+) -> Result<TwilioResponse, SMSError> {
+    sms_executor
+        .execute(from, to, account_id, access_token)
+        .map_err(|error| SMSError::ExecutionError {
+            error: error.to_string(),
+        })
+        .and_then(convert_to_string)
+        .and_then(|response| deserialize_twilio_response(&response))
+}
+
+fn convert_to_string(output: std::process::Output) -> Result<String, SMSError> {
+    String::from_utf8(output.stdout).map_err(|error| SMSError::TwilioResponseError {
+        error: error.to_string(),
+    })
+}
+
+fn deserialize_twilio_response(response: &str) -> Result<TwilioResponse, SMSError> {
+    serde_json::from_str(response).map_err(|error| SMSError::SerdeError {
+        error: error.to_string(),
+    })
+}
+
+pub struct CommandExecutor;
+
+impl SMSExecutor for CommandExecutor {
+    fn execute(
+        &self,
+        from: &str,
+        to: &str,
+        account_id: &str,
+        access_token: &str,
+    ) -> Result<std::process::Output, std::io::Error> {
+        Command::new("sh")
+            .arg("-c")
+            .arg(format!(
+                "
+        curl -X POST https://api.twilio.com/2010-04-01/Accounts/{}/Messages.json \
+            --data-urlencode \"Body=A Mariners Home Game is Starting\" \
+            --data-urlencode \"From=+1{}\" \
+            --data-urlencode \"To=+1{}\" \
+            -u {}:{}
+                    ",
+                account_id, from, to, account_id, access_token
+            ))
+            .output()
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
@@ -62,6 +108,118 @@ pub struct SubresourceURIs {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    struct SuccesfulMockExecutor;
+    struct FailingMockExecutor;
+
+    impl SMSExecutor for SuccesfulMockExecutor {
+        fn execute(
+            &self,
+            _: &str,
+            _: &str,
+            _: &str,
+            _: &str,
+        ) -> Result<std::process::Output, std::io::Error> {
+            let data = r#"
+            {
+                "account_sid": "ABCD1234",
+                "api_version": "2010-04-01",
+                "body": "Hello! 👍",
+                "date_created": "Thu, 30 Jul 2015 20:12:31 +0000",
+                "date_sent": "Thu, 30 Jul 2015 20:12:33 +0000",
+                "date_updated": "Thu, 30 Jul 2015 20:12:33 +0000",
+                "direction": "outbound-api",
+                "error_code": null,
+                "error_message": null,
+                "from": "+14155552345",
+                "messaging_service_sid": "MGXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+                "num_media": "0",
+                "num_segments": "1",
+                "price": -0.00750,
+                "price_unit": "USD",
+                "sid": "MMXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+                "status": "sent",
+                "subresource_uris": {
+                    "media": "/2010-04-01/Accounts/ABCD1234/Messages/SMXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Media.json"
+                },
+                "to": "+14155552345",
+                "uri": "/2010-04-01/Accounts/ABCD1234/Messages/SMXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX.json"
+            }"#;
+
+            Ok(std::process::Output {
+                stdout: data.as_bytes().to_vec(),
+                stderr: vec![],
+                status: std::os::unix::process::ExitStatusExt::from_raw(0),
+            })
+        }
+    }
+
+    impl SMSExecutor for FailingMockExecutor {
+        fn execute(
+            &self,
+            _: &str,
+            _: &str,
+            _: &str,
+            _: &str,
+        ) -> Result<std::process::Output, std::io::Error> {
+            let custom_error = std::io::Error::new(std::io::ErrorKind::Other, "oh no!");
+            Err(custom_error)
+        }
+    }
+
+    #[test]
+    fn different_executor_works_with_success_flow() {
+        let response = send_text_message(
+            "1234567890",
+            "0987654321",
+            "ABCD1234",
+            "A1B2C3D4",
+            &SuccesfulMockExecutor,
+        )
+        .unwrap();
+        assert_eq!(TwilioResponse::SendMessage {
+            account_sid: "ABCD1234".to_string(),
+            api_version: "2010-04-01".to_string(),
+            body: "Hello! 👍".to_string(),
+            date_created: "Thu, 30 Jul 2015 20:12:31 +0000".to_string(),
+            date_sent: Some("Thu, 30 Jul 2015 20:12:33 +0000".to_string()),
+            date_updated: "Thu, 30 Jul 2015 20:12:33 +0000".to_string(),
+            direction: "outbound-api".to_string(),
+            error_code: None,
+            error_message: None,
+            from: "+14155552345".to_string(),
+            messaging_service_sid: Some("MGXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX".to_string()),
+            num_media: "0".to_string(),
+            num_segments: "1".to_string(),
+            price: Some(-0.00750),
+            price_unit: "USD".to_string(),
+            sid: "MMXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX".to_string(),
+            status: "sent".to_string(),
+            subresource_uris: SubresourceURIs {
+                media: "/2010-04-01/Accounts/ABCD1234/Messages/SMXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Media.json".to_string()
+            },
+            to: "+14155552345".to_string(),
+            uri: "/2010-04-01/Accounts/ABCD1234/Messages/SMXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX.json".to_string()
+        }, response);
+    }
+
+    #[test]
+    fn different_executor_works_with_fail_flow() {
+        let response = send_text_message(
+            "1234567890",
+            "0987654321",
+            "ABCD1234",
+            "A1B2C3D4",
+            &FailingMockExecutor,
+        )
+        .err();
+        assert_eq!(
+            Some(SMSError::ExecutionError {
+                error: "oh no!".to_string(),
+            }),
+            response
+        );
+    }
 
     #[test]
     fn it_works() {
